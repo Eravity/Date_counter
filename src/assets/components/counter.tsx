4854f5bc222742b6e152a5f5a7d6d7d5fd194634
Cristian <81909682+Eravity@users.noreply.github.com>
2024-03-21 16:36:50 +0200

    counter.tsx

--- src/assets/components/counter.tsx
+++ src/assets/components/counter.tsx
@@ -20,42 +20,27 @@ const Step: React.FC = (): JSX.Element => {
   return (
     <>
       <div className="flex gap-3 items-center border-2 rounded-md px-2 py-1 border-neutral-600 bg-neutral-800">
-        <button
-          className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md hover:bg-[#383838] text-neutral-300 duration-200"
-          onClick={() => setStep((step) => step - 1)}
-        >
-          -
-        </button>
+        <button className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md hover:bg-[#383838] text-neutral-300 duration-200"
+          onClick={() => setStep((step) => step - 1)}> - </button>
 
         <h1 className="text-xl text-neutral-300 font-semibold">Step: {step}</h1>
 
-        <button
-          className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md text-neutral-300 hover:bg-[#383838] duration-200"
-          onClick={() => setStep((step) => step + 1)}
-        >
-          +
-        </button>
+        <button className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md text-neutral-300 hover:bg-[#383838] duration-200"
+          onClick={() => setStep((step) => step + 1)} > + </button>
       </div>
+      
       <p className="text-xl text-neutral-400">{formattedDate}</p>
-
+      
       <div className="flex gap-3 items-center border-2 rounded-md px-2 py-1 border-neutral-600 bg-neutral-800">
-        <button
-          className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md hover:bg-[#383838] text-neutral-300 duration-200"
-          onClick={() => setCount((count) => count - step)}
-        >
-          -
-        </button>
+        <button className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md hover:bg-[#383838] text-neutral-300 duration-200"
+          onClick={() => setCount((count) => count - step)} > - </button>
 
         <h1 className="text-xl text-neutral-300 font-semibold">
           count: {Math.abs(count)} days {count < 0 && "ago"}
         </h1>
 
-        <button
-          className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md text-neutral-300 hover:bg-[#383838] duration-200"
-          onClick={() => setCount((count) => count + step)}
-        >
-          +
-        </button>
+        <button className="pb-1 bg-neutral-700 w-6 h-6 flex items-center justify-center rounded-md text-neutral-300 hover:bg-[#383838] duration-200"
+          onClick={() => setCount((count) => count + step)} > + </button>
       </div>
     </>
   );
